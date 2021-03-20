dcd9ff87a5d5b5c7185d07975a68a2834cdbec9d
ahmet2021 <yilmazsrahmet@gmail.com>
2021-03-20 21:53:17 +0300

    first commit

---
+++ ironbank/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import React from "react";
+import{ BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Register from "./register/Register";
+
+function App() {
+  return (    
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/" component={Home}/>                          
+          </Switch>
+          <Footer />
+        </div>
+      </Router>    
+  );
+}
+
+export default App; 
